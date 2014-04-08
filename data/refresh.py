4569254e27aee089d6aef36e3f11cbe9227a4e70
Pavol Rusnak <stick@gk2.sk>
2014-04-08 22:32:42 +0200

    fix 52.1989256 showing as 52.198925299999999

--- data/refresh.py
+++ data/refresh.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import json
+import json.encoder
 from overpass import parser as overpass_parser
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
@@ -23,4 +24,5 @@ for name, parser in parsers.iteritems():
 	for coin in coins:
 		coin = coin.lower()
 		pts = parser.get_points(coin)
+		json.encoder.FLOAT_REPR = lambda x: str(x) # fix for 52.1989256 showing as 52.198925299999999
 		json.dump(pts, open(scriptdir + '/data-%s-%s.json' % (name, coin), 'w'), separators = (',', ':'))
